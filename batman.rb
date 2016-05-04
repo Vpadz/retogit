5f16f35227f8eb7bc1da579be65a79c34b8c9828
SolidXnake <innocenteagostinelli@gmail.com>
2016-05-03 21:54:52 -0430

    Added Methods ShowStats and Walk

--- batman.rb
+++ batman.rb
@@ -2,7 +2,28 @@ class Batman
 	def initialize 
 		@name =  "Batman"
 		@health = 30
-		@stage = [0]
+		@stage = []
+		@position=0
 	end
-	
+
+	def show_stats
+		puts("+--------------Batman's Stats-----------+")
+		puts("+--------------Health:#{@health} points---------+")
+		puts("+--------------Position:#{@position}---------------+")
+
+	end
+
+	def walk 
+		@position += 1
+		if position < 10
+			case @stage[position]
+			when 0
+				"Empty stage. You have run with luck this time Batman"
+			when 1
+				fight_noob
+			end
+		else
+			fight_boss
+		end
+	end
 end
